6f45bc5020d22d469477b9bd8466146ee13217aa
Gavin Orland <gavin@gavinorland.com>
2018-08-23 12:37:40 +0100

    Add comment

--- packages/storybook/src/stories/index.js
+++ packages/storybook/src/stories/index.js
@@ -4,6 +4,7 @@ import 'normalize.css';
 import './styles.css';
 import readme from '../../../../README.md';
 
+// dangerouslySetInnerHTML used below since 'readme' arrives as an HTML string that needs to be rendered into the DOM.
 // eslint-disable-next-line react/no-danger
 storiesOf(' Welcome', module).add('to govuk-frederic', () => <div style={{ padding: '10px' }} className="markdown-body" dangerouslySetInnerHTML={{ __html: readme }} />);
 
